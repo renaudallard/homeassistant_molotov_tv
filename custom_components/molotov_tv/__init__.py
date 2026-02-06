a4f2b333232dbf2f566551214535c143b44c3205
Renaud Allard <renaud@allard.it>
2026-02-06 08:19:09 +0100

    bump panel cache version to 0.1.16

--- custom_components/molotov_tv/__init__.py
+++ custom_components/molotov_tv/__init__.py
@@ -124,7 +124,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             webcomponent_name="molotov-panel",
             sidebar_title=PANEL_TITLE,
             sidebar_icon=PANEL_ICON,
-            module_url="/molotov_tv/www/molotov-panel.js?v=0.1.15",
+            module_url="/molotov_tv/www/molotov-panel.js?v=0.1.16",
             embed_iframe=False,
             require_admin=False,
             config={
